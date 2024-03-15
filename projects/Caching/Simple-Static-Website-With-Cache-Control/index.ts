dd88c92a21de29a13091c6f85ca87803b93f921b
Mahdi <86781212+MahdiNajafzadeh@users.noreply.github.com>
2024-03-16 01:31:13 +0330

    [Add]: Simple Web view for wallpapler website

--- projects/Caching/Simple-Static-Website-With-Cache-Control/index.ts
+++ projects/Caching/Simple-Static-Website-With-Cache-Control/index.ts
@@ -1,8 +1,9 @@
+console.clear();
 import express from "express";
 import type { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import path from "path";
-import { existsSync } from "node:fs";
+import { existsSync, readdirSync } from "node:fs";
 
 import cacheControll from "./middleware/cache.controll";
 
@@ -10,30 +11,36 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+const images = readdirSync("./views/image").map((v) => {
+	return { id: v.split(".")[0] };
+});
+
 const app = express();
 app.use(cacheControll);
-
-app.use(express.static(path.join(__dirname, 'views')));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-	res.redirect("/index.ejs");
-});
-
 app.get("/*", (req: Request, res: Response, next: NextFunction) => {
-	const pathFile = path.join(__dirname, "views", req.path);
+	const pathFile = path.join(__dirname, "views", req.path !== "/" ? req.path : "index");
 	if (!(existsSync(pathFile + ".ejs") || existsSync(pathFile + ".html"))) return next();
 
-	res.locals.title = req.query?.image ? `image : ${req.query?.image}` : "background images";
-	// res.locals.image = req.query?.image || "";
+	res.locals.title = req.path === '/image' ? `image : ${req.query?.id}` : "Wallpaper-World";
+	res.locals.query = req.query || "";
+	res.locals.images = images;
 
 	try {
 		res.render(pathFile + ".ejs");
 	} catch (error) {
 		console.error(error);
+		res.redirect("/error");
 	}
 });
 
-app.listen(PORT, () => console.log(`SERVER START SUCCESS :  http://localhost:${PORT}`));
+app.get("/error", (req: Request, res: Response, next: NextFunction) => {
+	res.render("./views/error.ejs");
+});
+
+app.use(express.static(path.join(__dirname, "views")));
+
+app.listen(PORT, () => console.log(`SERVER START SUCCESS : http://localhost:${PORT}`));
